230ab18661fe2dcf56737101a02a7a4ab8588b9b
Dimitri Lopez <dimitrilopez01@gmail.com>
2022-04-30 22:22:34 -0400

    Adding a writeup.

--- src/fancy_print.py
+++ src/fancy_print.py
@@ -9,11 +9,5 @@ class FancyPrint:
 
     def addRow(self, first, second):
         self.rows.append([first, second])
-        self.print()
     def print(self):
-        # for i in range(len(self.rows)):
-        #     self.rows[i][0] = toSpecialChar(self.rows[i][0])
         print(tabulate(self.rows, colalign = ("right",)))
-        # for i in self.rows:
-        #     print(i[1] + ": " + i[0])
-
